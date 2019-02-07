1e78d6845371874f256f456b39e7e927e8ffb88a
Robert <robertwinstonanderson@gmail.com>
2019-02-06 23:50:11 -0700

    Add api routes for post user preferences, and get survey detail

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -36,8 +36,27 @@ module.exports = function(app) {
   });
 
   //Post user preferences
+  app.post("/api/pref/:userId/:prefId", function(req, res) {
+    db.UserPreference.create({
+      UserId: req.params.userId,
+      MoviePrefId: req.params.prefId
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
 
   //Get SurveyDetail
+  app.get("/api/sdetail/:surveyId/:questionId/:answer", function(req, res) {
+    db.SurveyDetail.findAll({
+      where: {
+        SurveyId: req.params.surveyId,
+        QuestionId: req.params.surveyId,
+        Answer: req.params.answer
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
 
   //http://www.omdbapi.com/?apikey=trilogy&t=Captain+Marvel&y=2019
 };
