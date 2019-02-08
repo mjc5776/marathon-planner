e507aede85698cc8c7870607e5b805c5afc886bf
Robert <robertwinstonanderson@gmail.com>
2019-02-07 20:27:54 -0700

    Update History model

--- models/UserHistory.js
+++ models/UserHistory.js
@@ -1,14 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
   var UserHistory = sequelize.define("UserHistory", {
     UserId: DataTypes.INTEGER,
-    TitleYear: {
+    omdbId: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
-    },
-    SurveyId: DataTypes.INTEGER
+    }
   });
 
   //Pass UP ID into User table
@@ -17,15 +16,6 @@ module.exports = function(sequelize, DataTypes) {
       foreignKey: {
         allowNull: false
       }
-    });
-  };
-
-  //Pass id DOWN as FK to many Survey entries
-  UserHistory.associate = function(models) {
-    UserHistory.hasMany(models.Survey, {
-      // as: "histId",
-      // foreignKey: "id",
-      onDelete: "cascade"
     });
   };
 
