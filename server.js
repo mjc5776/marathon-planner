4fc4e764337e05991753bcf45710e4e9d6f739f5
Robert <robertwinstonanderson@gmail.com>
2019-02-09 12:31:31 -0700

    Auto stash before merge of "Staging" and "origin/Staging"

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ require("./routes/htmlRoutes")(app);
 // PASSPORT: load passport strategies
 require("./config/passport.js")(passport, models.user);
 
-const syncOptions = { force: false };
+const syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
