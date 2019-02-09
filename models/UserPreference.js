01fcbf260ce0d38c362fa492e25de547e6806295
Robert <robertwinstonanderson@gmail.com>
2019-02-09 13:55:41 -0700

    Update models, and add post for user history api

--- models/UserPreference.js
+++ models/UserPreference.js
@@ -1,12 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
   var UserPreference = sequelize.define("UserPreference", {
     UserId: DataTypes.INTEGER,
-    MoviePrefId: DataTypes.INTEGER
+    MovieGenreId: DataTypes.INTEGER
   });
 
   //Pass UP ID into User table
   UserPreference.associate = function(models) {
     UserPreference.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+
+    UserPreference.belongsTo(models.Genre, {
       foreignKey: {
         allowNull: false
       }
