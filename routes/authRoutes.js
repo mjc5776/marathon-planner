ebb9f521e6f3c9c1f3e23a2464603d3843c1b2ec
Mike Corey <mjcorey57@gmail.com>
2019-02-12 20:49:17 -0700

    Change dashboard to preference redirect

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -23,7 +23,7 @@ module.exports = (app, passport) => {
   app.post(
     "/signin",
     passport.authenticate("local-signin", {
-      successRedirect: "/dashboard",
+      successRedirect: "/preference",
       failureRedirect: "/signin"
     })
   );
@@ -47,9 +47,9 @@ module.exports = (app, passport) => {
     });
   });
 
-  // Load dashboard page after authentication
-  app.get("/dashboard", isLoggedIn, (req, res) => {
-    res.render("dashboard");
+  // Load preference page after authentication
+  app.get("/preference", isLoggedIn, (req, res) => {
+    res.render("preference");
   });
 
   // Passport function that checks if the user is logged in or not.  If not then it redirects them to the signin page
