978659c7c3ebae26b5d39234f4198a261df64675
Robert <robertwinstonanderson@gmail.com>
2019-02-14 23:24:11 -0700

    Add jQuery to populate cards

--- models/SurveyResults.js
+++ models/SurveyResults.js
@@ -1,5 +1,5 @@
-module.exports = function(sequelize, DataTypes) {
-  var SurveyResults = sequelize.define("SurveyResults", {
+module.exports = function (sequelize, DataTypes) {
+  var SurveyResults = sequelize.define('SurveyResults', {
     SurveyId: DataTypes.INTEGER,
     QuestionId: DataTypes.INTEGER,
     UserId: DataTypes.INTEGER,
